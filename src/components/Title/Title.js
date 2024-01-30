f1e789b77f86c2460fa19c2a629656e7a06235af
Harsh Gupta <67004713+Hartz-byte@users.noreply.github.com>
2024-01-30 22:43:50 +0530

    Update Title.js

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Title = () => {
-  return <div className="title">Demo-Editor by Harsh Gupta</div>;
+  return (
+    <div>
+      <div className="title">Demo-Editor by Harsh Gupta</div>
+      <p>Typing "# " will convert into Heading (h1)</p>
+      <p>Typing "* " will convert into bold text</p>
+      <p>Typing "** " will convert the text color to red</p>
+      <p>Typing "*** " will convert text style underline</p>
+    </div>
+  );
 };
 
 export default Title;
